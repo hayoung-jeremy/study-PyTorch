af162fea80fea64fb37153f04908eecafb867f23
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-05-22 18:00:43 +0900

    feat : Create a model

--- main.py
+++ main.py
@@ -24,3 +24,37 @@ for X, y in test_dataloader:
     print(f"Shpae of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y : {y.shape} {y.dtype}")
     break
+
+# 학습에 사용할 CPU, GPU, 또는 MPS 장치 불러오기
+device = torch.device(
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+print(f"Using {device} device")
+
+
+# define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork().to(device)
+print(model)
